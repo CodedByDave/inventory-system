2bf9949849be0cbecf4cb558c2dbb498d92e3aaa
Dave Magno <davemagno3011@gmail.com>
2025-09-27 20:19:31 +0800

    add comment for repository methods for better understanding

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -16,50 +16,55 @@ use function PHPSTORM_META\map;
 abstract class Repository
 {
     /**
-     * avoid boilerplate code in child repositories
+     * base repo constructor
      */
     public function __construct(protected Model $model) {}
 
+    // return model
     public function Model(): Model
     {
         return $this->model;
     }
 
+    // start query
     public function query(): Builder
     {
         return $this->model->query();
     }
 
+    // paginate data
     public function paginate(int $perPage = 15, array $columns = ['*'], array|string $relations = []): LengthAwarePaginator
     {
         return $this->model->with($relations)->paginate($perPage, $columns);
     }
 
+    // find by id
     public function find(int $id, array $columns = ['*'], array|string $relations = []): ?Model
     {
         return $this->model->with($relations)->find($id, $columns);
     }
 
+    // create record
     public function create(array $data): Model
     {
         return $this->model->create($data);
     }
 
+    // update record
     public function update(Model $model, array $data): bool
     {
-        return $this->model->update($data);
+        return $model->update($data);
     }
 
+    // update or create record
     public function updateOrCreate(array $attributes, array $values = []): Model
     {
         return $this->model->updateOrCreate($attributes, $values);
     }
 
-    public function delete(Model $model):bool
+    // delete record
+    public function delete(Model $model): bool
     {
-        return $this->model->delete();
+        return $model->delete();
     }
-
-
 }
-
